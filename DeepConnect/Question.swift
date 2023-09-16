898c1168420616613afadd20492d1bb74d499fd2
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-16 18:01:19 +0100

    Add documentation to Question struct

--- DeepConnect/Question.swift
+++ DeepConnect/Question.swift
@@ -1,6 +1,12 @@
 import Foundation
 
+/**
+ An object representing a question.
+
+ Primarily displayed in ``CardView``.
+ */
 struct Question: Identifiable {
+  // MARK: - Properties
   let id = UUID()
   let prompt: String
 }
