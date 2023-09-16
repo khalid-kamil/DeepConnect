1f25fcce2d82c77f32922aad9b0a0bdf51f4cc71
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-16 17:35:10 +0100

    Add SwiftLint to project and fix indentation

--- DeepConnect/DeepConnectApp.swift
+++ DeepConnect/DeepConnectApp.swift
@@ -1,17 +1,10 @@
-//
-//  DeepConnectApp.swift
-//  DeepConnect
-//
-//  Created by Khalid Kamil on 16/09/2023.
-//
-
 import SwiftUI
 
 @main
 struct DeepConnectApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
