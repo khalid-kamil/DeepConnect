ea066a75f25726e8bfc5a01541b33974b4b96615
Khalid Kamil <khalidkamil28@gmail.com>
2023-09-16 16:04:26 +0100

    Initial Commit

---
+++ DeepConnect/DeepConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepConnectApp.swift
+//  DeepConnect
+//
+//  Created by Khalid Kamil on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeepConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
